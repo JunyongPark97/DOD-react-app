cedd3ea917c0bd8261285c2640f45e281b3f4f68
Gtwr18 <dltkddn0323@snu.ac.kr>
2021-06-29 12:09:30 +0900

    [fixed]dod more link

--- src/network/network.js
+++ src/network/network.js
@@ -4,5 +4,5 @@ const baseUrl = 'https://docs.gift'
 export const kakaoLink = 'http://pf.kakao.com/_nfxcTs/chat'
 export const privacyPolicy = 'https://www.notion.so/dod-8dcf79f6f2e243f1a7b506b538a5ed92'
 export const useOfTerm = 'https://www.notion.so/dod-ad65925f9dc94279aa2b16cedd54bf3c'
-export const dodMoreLink = 'https://www.d-o-d.io/?utm_source=dod&utm_medium=reponse_page&utm_campaign=respondent'
+export const dodMoreLink = 'https://dod-beta.com/?utm_source=dod&utm_medium=service&utm_campaign=respondent'
 export default baseUrl;
