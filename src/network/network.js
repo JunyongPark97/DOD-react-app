cb61be73955aa3e5bfd3760f12893ee5e483b713
Gtwr18 <dltkddn0323@snu.ac.kr>
2021-06-29 13:46:03 +0900

    [Added]gtm event

--- src/network/network.js
+++ src/network/network.js
@@ -1,6 +1,7 @@
-const baseUrl = 'https://docs.gift'
+// const baseUrl = 'https://docs.gift'
 // const baseUrl = 'https://d-o-d.io'
 // const baseUrl = 'http://3.36.156.224:8000'
+const baseUrl = 'https://dod-link.com'
 export const kakaoLink = 'http://pf.kakao.com/_nfxcTs/chat'
 export const privacyPolicy = 'https://www.notion.so/dod-8dcf79f6f2e243f1a7b506b538a5ed92'
 export const useOfTerm = 'https://www.notion.so/dod-ad65925f9dc94279aa2b16cedd54bf3c'
