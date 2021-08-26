aed56dd3bda2b694125c4afd8b2e69ba981f3202
Gtwr18 <dltkddn0323@snu.ac.kr>
2021-08-26 21:03:03 +0900

    [FIXED] thankyou page speling

--- src/components/common/ThanksPage.js
+++ src/components/common/ThanksPage.js
@@ -16,7 +16,7 @@ export default function ThanksPage() {
                 감사합니다!
             </p>
             <p className='thanks-page-subtitle'>
-                실시간 추첨 설문이 올라오면<br/>카카오톡으로 알려드릴께요!
+                실시간 추첨 설문이 올라오면<br/>카카오톡으로 알려드릴게요!
             </p>
             <img alt='' src={process.env.PUBLIC_URL + '/alarm.png'} style={{width:'120px', height:'120px'}} className='thanks-page-img'/>
             <div className='result-modal-plz-container clickable' style={{backgroundColor:'#F0F5FF'}} onClick={onClickMore}>
